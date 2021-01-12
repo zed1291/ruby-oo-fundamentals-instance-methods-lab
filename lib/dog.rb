6202d276ceffb5e54b39ff6c5e4e1762505df184
“zed” <zed@kolk.me>
2021-01-11 19:08:05 -0600

    Defined Person and Dog classes and added things they can do

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+    def bark
+        p "Woof!"
+    end
+    def sit
+        p "The Dog is sitting"
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    def talk
+        p "Hello World!"
+    end
+    def walk
+        p "The Person is walking"
+    end
+end
